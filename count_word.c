ba368d1ddf44c78405270617b5c10a15b4efddd5
Tuan Anh <tuananh030305@gmail.com>
2024-10-20 23:46:09 +0700

    main

--- count_word.c
+++ count_word.c
@@ -36,28 +36,10 @@ int countWords(char *s)
 
 int main()
 {
-    char s[5000];  
-    char line[1000];
-
-    s[0] = '\0';
-
-    while(1)
-    {
-        fgets(line, sizeof(line), stdin);  // Đọc một dòng nhập từ người dùng
-        
-        // Nếu dòng vừa nhập chỉ là dấu xuống dòng (người dùng nhấn Enter mà không nhập gì)
-        if(strcmp(line, "\n") == 0)  
-        {
-            break;  // Thoát khỏi vòng lặp, kết thúc nhập
-        }
-        
-        strcat(s, line);  // Nối dòng vừa nhập vào chuỗi văn bản
-    }
-
-    // Gọi hàm đếm từ và in ra kết quả
-    int result = countWords(s);
-    printf("%d\n", result);
-
+   char s[4000];
+   fgets(s, sizeof(s), stdin);
+   int result = countWords(s);
+   printf("%d\n", result);
     return 0;
 }
 
